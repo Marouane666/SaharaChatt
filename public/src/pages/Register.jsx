6751ee6472ec44aaf82dfe5ac8c3109eb6c40fe6
Marouane666 <marouanetabaa@gmail.com>
2023-08-30 23:47:11 +0200

    Login system

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
@@ -8,6 +8,7 @@ import axios from "axios";
 import { registerRoute } from '../utils/APIRoutes';
 
 function Register() {
+    const navigate = useNavigate();
     const [values, setValues] = useState({
       username: "", 
       email: "",
@@ -28,12 +29,20 @@ const handleSubmit = async (event) => {
       event.preventDefault();
       if(handleValidation()){
         console.log("in validation...", registerRoute);
-        const { password, confirmPassword, username, email } = values;
+        const { password, username, email } = values;
         const {data} = await axios.post(registerRoute , {
           username,
           email,
           password,
         });
+        if(data.status === false){
+          toast.error(data.msg,toastOptions );
+          
+        }
+        if(data.status === true){
+          localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+        }
+        navigate();
       }
 };
 
@@ -56,6 +65,7 @@ const handleValidation = () => {
     toast.error("email please.",toastOptions );
     return false;
   }
+  console.log("tbon");
   return true;
 }
 
--- server/model/userModel.js
+++ server/model/userModel.js
@@ -2,7 +2,34 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: {
-        typr: String,
+        type: String,
         required: true,
-    }
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+
+    },
+    password : {
+        type: String,
+        required: true,
+        
+        max: 50,
+
+    },
+    isAvatarImageSet: {
+        type: Boolean,
+        default: false,
+    },
+    avatarImage : {
+        type: String,
+        default: "",
+    },
 });
+
+module.exports = mongoose.model("Users", userSchema);
